ece583c0c20d68113c71c4ffa8229de05cb068af
Dava Firmansyah <sindivegeance@gmail.com>
2021-10-27 02:02:45 +0700

    Add files via upload

---
+++ run-zcoin-2miners.sh
@@ -0,0 +1,4 @@
+echo -e '\033]2;MTP (XZC) - 2miners pool\007'
+./CryptoDredge -a mtp -o stratum+tcp://xzc.2miners.com:8080 -u a2VcGTRJ8g5NAWQ1pxYUcUVyHpWA2k9BEx -p x
+printf "Press <ENTER> to continue..."
+read -r continueKey
---
+++ run-zcoin-mintpond.sh
@@ -0,0 +1,4 @@
+echo -e '\033]2;MTP (XZC) - mintpond pool\007'
+./CryptoDredge -a mtp -o stratum+ssl://zcoin.mintpond.com:3005 -u a2VcGTRJ8g5NAWQ1pxYUcUVyHpWA2k9BEx -p x
+printf "Press <ENTER> to continue..."
+read -r continueKey
